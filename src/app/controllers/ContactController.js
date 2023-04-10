bd9447d013e7c36fb5d8b1df1b3642d72c0ea845
perfluka8760 <canalluka8760@gmail.com>
2023-04-10 00:40:10 -0300

    delete contact

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -18,11 +18,22 @@ class ContactController {
     response.json(contact);
   }
 
-  store() {}
+  async store(request, response) {}
 
   update() {}
 
-  delete() {}
+  async delete(request, response) {
+    const { id } = request.params;
+
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: "User not found" });
+    }
+
+    await ContactsRepository.delete(id);
+    response.sendStatus(204);
+  }
 }
 
 module.exports = new ContactController();
